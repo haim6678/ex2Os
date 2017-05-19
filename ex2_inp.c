d98da643df969d535f2851d6c2515ed8a7da2054
Haim Rubinstein <Haim6678@gmail.com>
2017-05-19 15:02:26 +0300

    need testing

--- ex2_inp.c
+++ ex2_inp.c
@@ -4,12 +4,14 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <fcntl.h>
 
 #define BUFF_SIZE 256
 #define MATRIX_ROW_SIZE 4
 #define MATRIX_COL_SIZE 4
+#define READ_SINE 79
 
-void PrintMatrix(int matrix[4][4]);
+void PrintMatrix();
 
 void HandleSiguser1(int sigNum);
 
@@ -17,90 +19,165 @@ void HandleClose(int sigNum);
 
 void GetFromStdin();
 
-int GameMatrix[MATRIX_ROW_SIZE][MATRIX_COL_SIZE] = {0};
+void SetSiguserSinal();
 
+int GameMatrix[MATRIX_ROW_SIZE][MATRIX_COL_SIZE] = {0};
+int fd;
+
+/**
+ *
+ * the output - 0 for success
+ * the operation - runs the program
+ */
 int main() {
 
+
+    fd = open("boardFile.txt", O_CREAT | O_RDONLY, 0666);
+    if (fd < 0) {
+        //todo handle
+    }
+
+    printf("%s \n", " create file in inp");
     //declare on the sigaction members
-    struct sigaction InputAction;
     struct sigaction CloseAction;
-    sigset_t inputBlock;
     sigset_t closeBlock;
 
     //fill the sett with all signals
-    sigfillset(&inputBlock);
     sigfillset(&closeBlock);
 
-    //remove the wanted signals
-    sigdelset(&inputBlock, SIGINT);
-    sigdelset(&inputBlock, SIGUSR1);//todo need this?
-    sigdelset(&closeBlock, SIGINT);
-
-    //set the handling function for siusr
-    InputAction.sa_handler = HandleSiguser1;
-    InputAction.sa_mask = inputBlock;
-    InputAction.sa_flags = 0;
     //set the handling function for sigint
-    CloseAction.sa_handler = HandleClose;
+
+    CloseAction.sa_handler = HandleClose; //todo need to reset after every handle??
     CloseAction.sa_mask = closeBlock;
     CloseAction.sa_flags = 0;
 
-    //set for sigusr1
-    if (sigaction(SIGUSR1, &InputAction, NULL) != 0) {
-        perror("faild in sigaction");
-        exit(-1);
-    }
+    //set for sigint
     if (sigaction(SIGINT, &CloseAction, NULL) != 0) {
         perror("faild in sigaction");
         exit(-1);
     }
 
-    PrintMatrix(GameMatrix);
+    SetSiguserSinal();
+    printf("%s \n", "start running inp");
+    //run the process indefinitely
+    while (1) {
+
+    }
     return 0;
 }
 
-void HandleSiguser1(int sigNum) {
-    GetFromStdin();
+/**
+ * the operation - this function define the beavihor of the handling
+ * in the siuser signal
+ */
+void SetSiguserSinal() {
+    struct sigaction InputAction;
+    sigset_t inputBlock;
+
+    sigfillset(&inputBlock);
+    //set the handling function for siusr
+    InputAction.sa_handler = HandleSiguser1;
+    InputAction.sa_mask = inputBlock;
+    InputAction.sa_flags = 0;
+
+    //remove the wanted signals
+    sigdelset(&inputBlock, SIGINT);
+
+    if (sigaction(SIGUSR1, &InputAction, NULL) != 0) {
+        perror("faild in sigaction");
+        exit(-1);
+    }
 }
 
-void HandleClose(int sigNum){
-    printf("%s","BYE BYE");
+/**
+ *
+ *the input - the sigNum
+ * the operation - this function is handling the recieve of the sigusr1 signal
+ */
+void HandleSiguser1(int sigNum) {
+    printf("%s \n", "got siguser signal");
+    SetSiguserSinal();
+    GetFromStdin();
+    PrintMatrix();
+}
+
+/**
+ *
+ *the input - the sigNum
+ * the operation - this function is handling the recieve of the sigint signal
+ */
+void HandleClose(int sigNum) {
+    printf("%s", "BYE BYE");
     exit(0);
 }
+
+int ConvertNum(char *buf) {
+    return atoi(buf);
+}
+
+/**
+ * the operation - after getting a signal we need to get the current board
+ * state,this function does this.
+ */
 void GetFromStdin() {
     int i = 0;
     int j = 0;
-    char buff[BUFF_SIZE];
-    if (read(STDIN_FILENO, buff, BUFF_SIZE) < 0) {
+    printf("%s \n", "reading from file inp");
+    char buff;
+    char num[5];
+    int number = 0;
+    int numLength = 0;
+    if (read(fd, &buff, 1) < 0) {
         //todo handle error
     }
-    char *numArr = strtok(buff, ",");
-
-    while ((numArr != NULL) && (i < MATRIX_ROW_SIZE * MATRIX_COL_SIZE)) {
-        GameMatrix[i][j] = atoi(numArr);
-
-        numArr = strtok(NULL, ",");
-
-        j++;
-        if (j % 4 == 0) {
-            j = 0;
-            i++;
+    while ((buff != '\0') && (buff != '\n')) {
+        if (buff == ',') {
+            number = ConvertNum(num);
+            numLength = 0;
+            memset(num, 0, 5);
+            GameMatrix[i][j] = number;
+            j++;
+            if (j == MATRIX_COL_SIZE) {
+                i++;
+                j = 0;
+            }
+            if (read(fd, &buff, 1) < 0) {
+                //todo handle error
+            }
+            continue;
+        }
+        if (numLength > 0) {
+            strcat(num, &buff);
+        } else {
+            strcpy(num, &buff);
+        }
+        numLength++;
+        if (read(fd, &buff, 1) < 0) {
+            //todo handle error
         }
     }
+
+
+    printf("%s \n", "finish parsing board inp");
 }
 
-void PrintMatrix(int matrix[4][4]) {
+/**
+ *
+ *   the operation - this function is in charge of printing the board to the game
+ */
+void PrintMatrix() {
     int i = 0;
     char temp[32];
     //todo handle write errors
+    printf("%s \n", "printing board");
     for (i; i < 4; i++) {
         write(STDOUT_FILENO, "|", strlen("|"));
         int j = 0;
         for (j; j < 4; j++) {
             printf("%s", " ");
-            if ((matrix[i][j]) > 0) {
+            if ((GameMatrix[i][j]) > 0) {
                 memset(temp, 32, 0);
-                sprintf(temp, "%04d", matrix[i][j]);
+                sprintf(temp, "%04d", GameMatrix[i][j]);
                 write(STDOUT_FILENO, temp, strlen(temp));
             } else {
                 write(STDOUT_FILENO, "    ", strlen("    "));
@@ -110,4 +187,5 @@ void PrintMatrix(int matrix[4][4]) {
         }
         write(STDOUT_FILENO, "\n", strlen("\n"));
     }
+    printf("%s \n", "finish printing board");
 }
